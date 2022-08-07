1155059bb556b0fbf117575be472f6768b8ae8cd
Veronika Sirak <7nicka@gmail.com>
2022-08-07 15:47:25 +0100

    add read fn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("home");
+const contactsOperations = require("./db/contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "read":
+      const data = await contactsOperations.listContacts();
+      console.log(data);
+      break;
+
+    default:
+      console.log("Unknow operation");
+      break;
+  }
+};
+
+invokeAction({ action: "read" });
