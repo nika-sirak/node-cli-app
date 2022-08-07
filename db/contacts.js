2cf744ec605867fe00e3bf96143c15a7af67d027
Veronika Sirak <7nicka@gmail.com>
2022-08-07 18:05:14 +0100

    add contacts operations

--- db/contacts.js
+++ db/contacts.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
@@ -8,6 +9,38 @@ const listContacts = async () => {
   return JSON.parse(data);
 };
 
+const getContactById = async (contactId) => {
+  const result = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(result);
+  const contact = contacts.find((item) => item.id === contactId);
+  return contact;
+};
+
+const removeContact = async (contactId) => {
+  const result = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(result);
+  const newContacts = contacts.filter((item) => item.id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+  return newContacts;
+};
+
+const addContact = async (name, email, phone) => {
+  const newContanct = {
+    id: uuidv4(),
+    name,
+    email,
+    phone,
+  };
+  const result = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(result);
+  contacts.push(newContanct);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContanct;
+};
+
 module.exports = {
   listContacts,
+  getContactById,
+  removeContact,
+  addContact,
 };
--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const contactsOperations = require("./db/contacts");
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case "read":
+    case "list":
       const data = await contactsOperations.listContacts();
       console.log(data);
       break;
-
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      console.log(contact);
+      break;
+    case "remove":
+      const newContacts = await contactsOperations.removeContact(id);
+      console.log(newContacts);
+      break;
+    case "add":
+      const newContact = await contactsOperations.addContact(
+        name,
+        email,
+        phone
+      );
+      console.log(newContact);
+      break;
     default:
-      console.log("Unknow operation");
+      console.warn("\x1B[31m Unknown action type!");
       break;
   }
 };
 
-invokeAction({ action: "read" });
+invokeAction({
+  action: "add",
+  name: "Mango Db",
+  email: "mango@gmail.com",
+  phone: "0958018728",
+});
